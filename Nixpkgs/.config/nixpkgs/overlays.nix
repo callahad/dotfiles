b6df3647ff04ef8272534b9a948e6e4ac2779978
Dan Callahan <dan.callahan@gmail.com>
2020-03-15 20:43:59 +0000

    Avoid network hit, use local nixpkgs-mozilla

--- Nixpkgs/.config/nixpkgs/overlays.nix
+++ Nixpkgs/.config/nixpkgs/overlays.nix
@@ -1,7 +1,7 @@
 [
-  (import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz))
+  #(import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz))
   # Use a local copy of nixpkgs-mozilla
-  #(import ./nixpkgs-mozilla/default.nix)
+  (import ./nixpkgs-mozilla/default.nix)
 
   (import ./my-packages.nix)
   (import ./other-packages/default.nix)
