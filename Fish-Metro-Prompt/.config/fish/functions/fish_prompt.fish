dba7247090761ee4e727577383a651d07570ec00
Dan Callahan <dan.callahan@gmail.com>
2020-08-08 16:53:16 +0100

    Fish prompt: Account for Git branches named main

--- Fish-Metro-Prompt/.config/fish/functions/fish_prompt.fish
+++ Fish-Metro-Prompt/.config/fish/functions/fish_prompt.fish
@@ -75,8 +75,8 @@ function fish_prompt
         set -l prompt
         set -l git_ahead (git_ahead "+" "-" "±")
 
-        if test "$branch_name" = master
-            set prompt "$git_glyph $git_ahead"
+        if test "$branch_name" = master -o "$branch_name" = main
+            set prompt "$git_glyph$git_ahead"
         else
             set prompt " $git_glyph $branch_name $git_ahead "
         end
