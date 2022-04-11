1c10836439e96726e70464df99b1beed12f1c66c
Dan Callahan <dan.callahan@gmail.com>
2022-04-11 12:56:50 +0100

    Neovim: Whitespace

--- Neovim/.config/nvim/init.lua
+++ Neovim/.config/nvim/init.lua
@@ -166,14 +166,13 @@ require('packer').startup(function(use)
             -- Use an on_attach function to only map the following keys
             -- after the language server attaches to the current buffer
             local on_attach = function(client, bufnr)
-                local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-                local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
                 -- Enable completion triggered by <c-x><c-o>
                 -- Note: Disabled in favor of using nvim-cmp below
+                --local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
                 --buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
                 -- Mappings.
+                local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
                 local opts = { noremap=true, silent=true }
 
                 -- See `:help vim.lsp.*` for documentation on any of the below functions
