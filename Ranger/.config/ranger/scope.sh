ceafe07229ee0199e8c1531ea9fc66a171268eca
Dan Callahan <dan.callahan@gmail.com>
2022-11-11 21:02:51 +0000

    Ranger: Update config
    rifle.conf is upstream with the addition of nsxiv.

--- Ranger/.config/ranger/scope.sh
+++ Ranger/.config/ranger/scope.sh
@@ -63,6 +63,9 @@ handle_extension() {
             ## Avoid password prompt by providing empty password
             7z l -p -- "${FILE_PATH}" && exit 5
             exit 1;;
+        rpa)
+            rpatool --list -- "${FILE_PATH}" && exit 5
+            exit 1;;
 
         ## PDF
         pdf)
